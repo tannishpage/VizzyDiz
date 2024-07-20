f52710d4cbf7fd719629efed8e506445be3c155b
Aravind Venkata Punugu <tannishpage1@gmail.com>
2024-07-20 19:10:17 +1000

    Changed name of function to generalize

--- VizzyDiz.py
+++ VizzyDiz.py
@@ -15,22 +15,65 @@ class AudioVisualizer:
         self._no_bins = no_bins
 
     def frequency_spectrum(self, x, sf):
-        pass
+        """
+        Derive frequency spectrum of a signal from time domain
+        :param x: signal in the time domain
+        :param sf: sampling frequency
+        :returns frequencies and their content distribution
+        """
+        x = x - np.average(x)  # zero-centering
 
-    def convert_to_bins(self, freq, x, no_bins=8):
-        pass
+        n = len(x)
+        k = np.arange(n)
+        tarr = n / float(sf)
+        frqarr = k / float(tarr)  # two sides frequency range
+
+        frqarr = frqarr[range(n // 2)]  # one side frequency range
+
+        x = fft.fft(x) / n  # fft computing and normalization
+        x = x[range(n // 2)]
+
+        return frqarr, abs(x)
+
+    def discretize(self, freq, x, no_bins=8):
+        """
+        Takes the fft values and reduces the number of bars by averaging
+        :param freq: the audio frequency
+        :param x: the audio frequency level
+        :param no_bins: the number of bars to convert to
+        :returns: np.arrays containing audio freq and levels with the desired bar count
+        """
+        chunck = (freq.size // no_bins) + 1 # The chunck size to average
+
+        freq_bins = np.zeros(no_bins)
+        x_bins = np.zeros(no_bins)
+
+        start = 0
+        for i, end in enumerate(range(start, freq.size, chunck)):
+            # Averaging levels of size chunck
+            x_bins[i] = min(x[start:end].mean(), 495) # Setting 495 as the maximum level
+            freq_bins[i] = freq[start:end].mean()
+            start = end
+        
+        return freq_bins, x_bins
 
     def update(self, frame):
         pass
 
     def _load_audio(self):
-        pass
+        self._audio = pydub.AudioSegment.from_mp3(self._audio_file)
 
     def setup_annimation(self):
         pass
 
     def setup_plot(self):
+        self.fig, self.ax = plt.subplots()
+        self.fig.set_size_inches(19.2, 10.8) # Output at 1920x1080 Resolution
+    
+    def setup_visualizer(self):
         pass
+
+
 
 class BarVisualizer(AudioVisualizer):
     pass
